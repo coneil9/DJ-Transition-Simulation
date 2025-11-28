75050823e31a1af3b1041838e503236e92e8f090
Carter O'Neil <carterrossoneil@gmail.com>
2025-11-27 16:34:15 -0800

    Needed to update Homebrew and install cmake.

---
+++ build/CMakeFiles/djtransition.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/carteroneil/Documents/DJ-Transition-Simulation/src/AudioLoader.cpp" "CMakeFiles/djtransition.dir/src/AudioLoader.cpp.o" "gcc" "CMakeFiles/djtransition.dir/src/AudioLoader.cpp.o.d"
+  "/Users/carteroneil/Documents/DJ-Transition-Simulation/src/BpmAnalyzer.cpp" "CMakeFiles/djtransition.dir/src/BpmAnalyzer.cpp.o" "gcc" "CMakeFiles/djtransition.dir/src/BpmAnalyzer.cpp.o.d"
+  "/Users/carteroneil/Documents/DJ-Transition-Simulation/src/EnergyAnalyzer.cpp" "CMakeFiles/djtransition.dir/src/EnergyAnalyzer.cpp.o" "gcc" "CMakeFiles/djtransition.dir/src/EnergyAnalyzer.cpp.o.d"
+  "/Users/carteroneil/Documents/DJ-Transition-Simulation/src/KeyAnalyzer.cpp" "CMakeFiles/djtransition.dir/src/KeyAnalyzer.cpp.o" "gcc" "CMakeFiles/djtransition.dir/src/KeyAnalyzer.cpp.o.d"
+  "/Users/carteroneil/Documents/DJ-Transition-Simulation/src/TransitionAnalyzer.cpp" "CMakeFiles/djtransition.dir/src/TransitionAnalyzer.cpp.o" "gcc" "CMakeFiles/djtransition.dir/src/TransitionAnalyzer.cpp.o.d"
+  "/Users/carteroneil/Documents/DJ-Transition-Simulation/src/main.cpp" "CMakeFiles/djtransition.dir/src/main.cpp.o" "gcc" "CMakeFiles/djtransition.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/djtransition.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/djtransition.dir/src/AudioLoader.cpp.o"
+  "CMakeFiles/djtransition.dir/src/AudioLoader.cpp.o.d"
+  "CMakeFiles/djtransition.dir/src/BpmAnalyzer.cpp.o"
+  "CMakeFiles/djtransition.dir/src/BpmAnalyzer.cpp.o.d"
+  "CMakeFiles/djtransition.dir/src/EnergyAnalyzer.cpp.o"
+  "CMakeFiles/djtransition.dir/src/EnergyAnalyzer.cpp.o.d"
+  "CMakeFiles/djtransition.dir/src/KeyAnalyzer.cpp.o"
+  "CMakeFiles/djtransition.dir/src/KeyAnalyzer.cpp.o.d"
+  "CMakeFiles/djtransition.dir/src/TransitionAnalyzer.cpp.o"
+  "CMakeFiles/djtransition.dir/src/TransitionAnalyzer.cpp.o.d"
+  "CMakeFiles/djtransition.dir/src/main.cpp.o"
+  "CMakeFiles/djtransition.dir/src/main.cpp.o.d"
+  "djtransition"
+  "djtransition.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/djtransition.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
